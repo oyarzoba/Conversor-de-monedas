f68a6b5bb60967f6005ec6cd06dd63157370641c
oyarzoba <62921883+oyarzoba@users.noreply.github.com>
2025-01-19 00:26:57 -0300

    Add files via upload

---
+++ Conversor-de-Monedas/README.md
@@ -0,0 +1,22 @@
+<p align="center">
+
+# Conversor de Monedas 
+
+Challenge Conversor de Monedas, propuesto por Alura Latam en colaboración con Oracle en el programa ONE, como parte de la especialización Back-End.
+
+## Descripción 
+
+Este proyecto es un Conversor de Monedas desarrollado en Java, con el fin de convertir diferentes divisas, utilizando una API de tasas de cambio en tiempo real. Con este conversor, se pueden realizar solicitudes a la API, analizar la respuesta JSON, filtrar las monedas de interés y mostrar los resultados a los usuarios de manera clara y concisa.
+
+## Tecnologías Utilizadas 
+
+- **Lenguaje de Programación:** Java
+- **API de Tasas de Cambio sugerida** 
+- **Biblioteca :** Gson
+- **Control de Versiones:** Git/GitHub 
+- **Entorno de Desarrollo Integrado (IDE):** IntelliJ IDEA 
+## Desarrollado por
+- Oscar Oyarzo Bassaletti
+
+¡Disfruta convirtiendo monedas!
+
---
+++ Conversor-de-Monedas/src/Principal.java
@@ -0,0 +1,92 @@
+import com.google.gson.JsonSyntaxException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        int opcionElegida = 0;
+
+        String menu = """
+                \n***************************************************
+                *** Bienvenido al Conversor de Monedas ***
+                
+                1) MXN ==>> USD
+                2) MXN ==>> EUR
+                3) MXN ==>> GBP
+                4) USD ==>> MXN
+                5) EUR ==>> MXN
+                6) GBP ==>> MXN
+                7) Otra conversión
+                8) Salir
+                \n***************************************************
+                """;
+        ConsultaConversion consulta = new ConsultaConversion();
+
+        Calculos calculos = new Calculos(consulta);
+        GeneradorDeArchivos generador = new GeneradorDeArchivos();
+
+        List<String> respuestas = new ArrayList<>();
+
+
+
+        while (opcionElegida != 8) {
+            try {
+                System.out.println(menu);
+                opcionElegida = Integer.parseInt(lectura.nextLine());
+
+
+                LocalDateTime myDateObj = LocalDateTime.now();
+                DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                String formattedDate = myDateObj.format(myFormatObj);
+
+                switch (opcionElegida) {
+                    case 1:
+                        calculos.almacenarValores("MXN", "USD");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 2:
+                        calculos.almacenarValores("MXN", "EUR");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 3:
+                        calculos.almacenarValores("MXN", "GBP");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 4:
+                        calculos.almacenarValores("USD", "MXN");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 5:
+                        calculos.almacenarValores("EUR", "MXN");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 6:
+                        calculos.almacenarValores("GBP", "MXN");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 7:
+                        calculos.almacenarValoresPersonalizados();
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 8:
+                        break;
+                    default:
+                        System.out.println("Ingrese una opción válida");
+                }
+            } catch (JsonSyntaxException | NullPointerException e) {
+                System.out.println("Error. Ingrese solo códigos de moneda válidos.");
+            } catch (NumberFormatException | InputMismatchException e) {
+                System.out.println("Error. Ingrese un valor numérico válido.");
+            }
+        }
+        generador.guardarJson(respuestas);
+
+        System.out.println("Finalizando programa. Gracias por preferirnos");
+    }
+}
